4299fc85d9b666eda85796b594c0070af76094c7
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-06-02 01:27:25 -0300

    calcular Melhor Distancia

--- Robo_Johnny.ino
+++ Robo_Johnny.ino
@@ -6,6 +6,7 @@
 #define pinTrig A0
 #define pinEcho A1
 #define pinBuzzer A2
+#define pinServo 10
 
 // DEFINIÇÕES DE PARAMETROS
 #define distObstaculo 10 // DISTANCIA EM CM PARA CONSIDERAR
@@ -21,14 +22,19 @@ bool medidaSensor[medidaDist]; // ARRAY PARA ARMAZENAR OS ULTIMOS VALORES
 byte posicao;
 byte contaObstaculo = 0;
 unsigned long controleLeitura; // VARIAVEL PARA CONTROLAR TEMPO ENTRE AS LEITURAS
+int angulo = 90;
+long duracao;
+long distanciaCm = 0;
 
 // INSTANCIANDO OBJETOS
 HCSR04 sensorHCSR04(pinTrig, pinEcho);
+Servo servo;
 
 // INSTANCIANDO METODOS
 bool validarLeitura(int tempo);
 bool validarParada(bool freio);
 void ligarBuzzer(bool freio);
+void posicionaCarroMelhorCaminho();
 
 void setup() {
   #ifdef DEBUG
@@ -39,6 +45,9 @@ void setup() {
   #endif
 
   pinMode(pinBuzzer, OUTPUT);
+  servo.attach(pinServo);
+
+  servo.write(angulo);
 
   for (byte i = 0; i < medidaDist; i++) {
     medidaSensor[i] = 0;
@@ -81,6 +90,23 @@ void loop() {
   }
 }
 
+void pensar() {
+  reposicionaServoSonar(); //Coloca o servo para olhar a frente
+
+  int distancia = lerSonar(); // Ler o sensor de distância
+
+  Serial.print("distancia em cm: "); 
+  Serial.println(distancia);   // Exibe no serial monitor 
+
+  if (distancia > distObstaculo) {  // Se a distância for maior que 20 cm  
+    //rotacao_Frente(); //robô anda para frente 
+  } else {
+    //rotacao_Parado();  //para o robô
+    posicionaCarroMelhorCaminho();
+    pensar();
+  }
+}
+
 bool validarLeitura(int tempo) {
   novaLeitura = false;
   
@@ -108,7 +134,7 @@ bool validarParada() {
 
       Serial.println("PARA!!!");
 
-      // tone(pinBuzzer, 2500, 100);
+      posicionaCarroMelhorCaminho();
     }
   } else {
     if (parar == true) {
@@ -123,8 +149,139 @@ bool validarParada() {
 
 void ligarBuzzer(bool freio) {
   if (freio) {
-    tone(pinBuzzer, 2500, 100);
+    tone(pinBuzzer, 1000, 100);
   } else {
     digitalWrite(pinBuzzer, LOW);
   }
+}
+
+void posicionaCarroMelhorCaminho() {
+  char melhorDist = calculaMelhorDistancia();
+  
+  Serial.print("Melhor Distancia em cm: ");
+  Serial.println(melhorDist);
+
+  switch(melhorDist) {
+    case 'c':
+      pensar();
+      break;
+    case 'd':
+      Serial.println("Virar a direita");
+      //rotacao_Direita();
+      break;
+    case 'e':
+      //rotacao_Esquerda();
+      Serial.println("Virar a esquerda");
+      break;
+  }
+
+  reposicionaServoSonar();
+}
+
+char calculaMelhorDistancia() {
+  char melhorDistancia = '0';
+  int maiorDistancia = 0;
+
+  int esquerda = calcularDistanciaEsquerda();
+  int centro = calcularDistanciaCentro();
+  int direita = calcularDistanciaDireita();
+
+  reposicionaServoSonar();
+
+  if (centro > direita && centro > esquerda){
+    melhorDistancia = 'c';    
+    maiorDistancia = centro; 
+  } else if (direita > centro && direita > esquerda){
+    melhorDistancia = 'd';
+    maiorDistancia = direita;
+  } else if (esquerda > centro && esquerda > direita){
+    melhorDistancia = 'e';
+    maiorDistancia = esquerda;
+  }
+
+  if (maiorDistancia <= distObstaculo) { //distância limite para parar o robô
+    // rotacao_Re();    
+    // posicionaCarroMelhorCaminho();
+  }
+
+  reposicionaServoSonar();
+
+  return melhorDistancia;
+}
+
+int calcularDistanciaCentro() {
+  servo.write(90);
+  
+  delay(20);
+  
+  int leituraDoSonar = lerSonar();  // Ler sensor de distância
+  
+  delay(500);
+  
+  leituraDoSonar = lerSonar();
+  
+  delay(500);
+  
+  Serial.print("Distancia do Centro: "); // Exibe no serial
+  Serial.println(leituraDoSonar);
+  
+  return leituraDoSonar;       // Retorna a distância
+}
+
+int calcularDistanciaDireita() {
+  servo.write(0);
+  
+  delay(200);
+  
+  int leituraDoSonar = lerSonar();
+  
+  delay(500);
+  
+  leituraDoSonar = lerSonar();
+  
+  delay(500);
+  
+  Serial.print("Distancia da Direita: ");
+  Serial.println(leituraDoSonar);
+  
+  return leituraDoSonar;
+}
+
+int calcularDistanciaEsquerda() {
+  servo.write(180);
+  
+  delay(200);
+  
+  int leituraDoSonar = lerSonar();
+  
+  delay(500);
+  
+  leituraDoSonar = lerSonar();
+  
+  delay(500);
+  
+  Serial.print("Distancia Esquerda: ");
+  Serial.println(leituraDoSonar);
+  
+  return leituraDoSonar;
+}
+
+int lerSonar() {
+  digitalWrite(pinTrig, LOW); // não envia som
+  delayMicroseconds(2);
+  digitalWrite(pinTrig, HIGH); // envia som
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, LOW); //não envia o som e espera o retorno do som enviado
+  
+  duracao = pulseIn(pinEcho, HIGH); //Captura a duração em tempo do retorno do som.
+
+  distanciaCm = duracao/56; //Calcula a distância;
+  
+  return distanciaCm;
+}
+
+void reposicionaServoSonar() {
+  servo.write(90);
+  
+  delay(200);
 }
