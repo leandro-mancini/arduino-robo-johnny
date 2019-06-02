f789352fd7246bbb0ac3e5da42c6f65522415ef7
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-06-01 21:34:30 -0300

    Projeto init

---
+++ Robo_Johnny.ino
@@ -0,0 +1,26 @@
+// DEFINIÇÕES DE BIBLIOTECAS.
+#include <Servo.h>  // inclui biblioteca de manipulação de servos motores. 
+#include <HCSR04.h> // inclui biblioteca ultrasonico.
+
+// DEFINIÇÕES DE PIENOS
+#define pinTrig 8
+#define pinEcho 9
+// #define pinBuzzer 4
+
+// DEFINIÇÕES DE PARAMETROS
+#define distObstaculo 30 // DISTANCIA EM CM PARA CONSIDERAR
+#define tempoLeitura 50 // INTERVALO MINIMO ENTRE CADA LEITURA
+#define medidaDist 5 // QUANTIDADE DE MEDIDAS QUE SERAO ARMAZENADAS
+
+#define DEBUG // DEIXAR DESCOMENTADO PARA RECEBER MAIS INFORMAÇÕES NO MONITOR SERIAL
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
