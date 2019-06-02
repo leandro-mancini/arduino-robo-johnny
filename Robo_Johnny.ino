da44000bf9ef397259e80ed7f79b92a6b23a0679
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-06-01 22:26:00 -0300

    Sensor ultrassonico - Medindo distancia

--- Robo_Johnny.ino
+++ Robo_Johnny.ino
@@ -14,13 +14,89 @@
 
 #define DEBUG // DEIXAR DESCOMENTADO PARA RECEBER MAIS INFORMAÇÕES NO MONITOR SERIAL
 
+// DECLARAÇÃO DE VARIAVEIS
+bool obstaculo = false;
+bool parar = false;
+bool novaLeitura;
+bool medidaSensor[medidaDist]; // ARRAY PARA ARMAZENAR OS ULTIMOS VALORES
+byte posicao;
+byte contaObstaculo = 0;
+unsigned long controleLeitura; // VARIAVEL PARA CONTROLAR TEMPO ENTRE AS LEITURAS
+
+// INSTANCIANDO OBJETOS
+HCSR04 sensorHCSR04(pinTrig, pinEcho);
+
 
 void setup() {
-  // put your setup code here, to run once:
+  #ifdef DEBUG
+    Serial.begin(9600);
+    Serial.println(F("| ============================================ |"));
+    Serial.println(F("| Arduino com sensor de obstáculo Ultrassonico |"));
+    Serial.println(F("| ============================================ |"));
+  #endif
 
+  for (byte i = 0; i < medidaDist; i++) {
+    medidaSensor[i] = 0;
+
+    #ifdef DEBUG
+      Serial.print("Setando posição ");
+      Serial.print(i);
+      Serial.println(" do array como");
+    #endif
+  }
+
+  posicao = 0;
+
+  #ifdef DEBUG
+    Serial.println("Fim do Setup");
+  #endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  novaLeitura = false;
 
+  if (millis() - controleLeitura > tempoLeitura) {
+    if (sensorHCSR04.dist() <= distObstaculo) {
+      obstaculo = true;
+      novaLeitura = true;
+    } else {
+      obstaculo = false;
+      novaLeitura = true;
+    }
+
+    controleLeitura = millis();
+  }
+
+  if (novaLeitura == true) {
+    medidaSensor[posicao] = obstaculo;
+    posicao++;
+
+    for (byte i = 0; i < medidaDist; i++) {
+      if (medidaSensor[i] == 1) contaObstaculo++;
+    }
+
+    if (contaObstaculo >= ((medidaDist/2) + 1)) {
+      if (parar == false) {
+        parar = true;
+
+        Serial.println("PARA!!!");
+
+        // Emit alguma coisa aqui.
+      }
+    } else {
+      if (parar == true) {
+        parar = false;
+
+        Serial.println("Caminho Livre");
+
+        // Emit alguma coisa aqui.
+      }
+    }
+
+    contaObstaculo = 0;
+
+    if(posicao > medidaDist) {
+      posicao = 0;
+    }
+  }
 }
